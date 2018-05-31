a68308fa16ff22841783b4c61b49c5ce2cc5e0a8
w.vela <boywang004@hotmail.com>
2018-05-31 11:13:31 +0800

    [code healthy] add deprecated for withDefaultValue, coz mostly abuse. (#2)
    * [code healthy] add deprecated for withDefaultValue, coz mostly abuse.

--- src/main/java/com/github/phantomthief/scope/ScopeKey.java
+++ src/main/java/com/github/phantomthief/scope/ScopeKey.java
@@ -1,10 +1,16 @@
 package com.github.phantomthief.scope;
 
 import static com.github.phantomthief.scope.Scope.getCurrentScope;
+import static com.google.common.collect.Lists.newArrayList;
+import static java.lang.Thread.currentThread;
 
+import java.util.List;
 import java.util.function.Supplier;
 
 import javax.annotation.Nonnull;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * 强类型数据读写的封装
@@ -13,6 +19,7 @@ import javax.annotation.Nonnull;
  */
 public final class ScopeKey<T> {
 
+    private static final Logger logger = LoggerFactory.getLogger(ScopeKey.class);
     private final T defaultValue;
     private final Supplier<T> initializer;
 
@@ -23,15 +30,45 @@ public final class ScopeKey<T> {
 
     @Nonnull
     public static <T> ScopeKey<T> allocate() {
-        return withDefaultValue(null);
+        return (ScopeKey<T>) withDefaultValue((Object) null);
     }
 
     /**
      * @param defaultValue 如果未执行 {@link #set} 或者在非Scope，调用 {@link #get} 返回的默认值
+     * 请使用 {@link #withInitializer(Supplier)} 或者其它重载版本代替
      */
     @Nonnull
+    @Deprecated
     public static <T> ScopeKey<T> withDefaultValue(T defaultValue) {
+        outputDeprecatedLog();
         return new ScopeKey<>(defaultValue, null);
+    }
+
+    @Nonnull
+    public static ScopeKey<Boolean> withDefaultValue(boolean defaultValue) {
+        return withDefaultValue(Boolean.valueOf(defaultValue));
+    }
+
+    @Nonnull
+    public static ScopeKey<Integer> withDefaultValue(int defaultValue) {
+        return withDefaultValue(Integer.valueOf(defaultValue));
+    }
+
+    @Nonnull
+    public static ScopeKey<Long> withDefaultValue(long defaultValue) {
+        return withDefaultValue(Long.valueOf(defaultValue));
+    }
+
+    @SuppressWarnings("unchecked")
+    @Nonnull
+    public static ScopeKey<String> withDefaultValue(String defaultValue) {
+        return (ScopeKey) withDefaultValue((Object) defaultValue);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Nonnull
+    public static <T extends Enum<T>> ScopeKey<T> withDefaultValue(T defaultValue) {
+        return (ScopeKey) withDefaultValue((Object) defaultValue);
     }
 
     /**
@@ -78,6 +115,29 @@ public final class ScopeKey<T> {
             return true;
         } else {
             return false;
+        }
+    }
+
+    private static void outputDeprecatedLog() {
+        List<StackTraceElement> stackTrace = newArrayList(currentThread().getStackTrace());
+        boolean[] after = { false };
+        int[] count = { 0 };
+        String location = stackTrace.stream() //
+                .filter(stack -> {
+                    if (stack.getClassName().equals(ScopeKey.class.getName())) {
+                        count[0]++;
+                        after[0] = true;
+                        return false;
+                    }
+                    return after[0];
+                }) //
+                .map(stack -> stack.getFileName() + ":" + stack.getLineNumber()) //
+                .findFirst() //
+                .orElse(null);
+
+        if (count[0] == 2) {
+            logger.warn("found deprecated call ScopeKey.withDefaultValue at location:({})",
+                    location);
         }
     }
 }
---
+++ src/test/java/com/github/phantomthief/scope/ScopeKeyTest.java
@@ -0,0 +1,48 @@
+package com.github.phantomthief.scope;
+
+import static com.github.phantomthief.scope.ScopeKey.allocate;
+import static com.github.phantomthief.scope.ScopeKey.withDefaultValue;
+import static com.github.phantomthief.scope.ScopeKeyTest.TestEnum.ABC;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author w.vela
+ * Created on 2018-05-30.
+ */
+class ScopeKeyTest {
+
+    private static final ScopeKey<List<String>> LIST_SCOPE_KEY = withDefaultValue(
+            new ArrayList<>());
+    private static final ScopeKey<Integer> INT_SCOPE_KEY = withDefaultValue(1);
+    private static final ScopeKey<Long> LONG_SCOPE_KEY = withDefaultValue(1L);
+    private static final ScopeKey<String> STRING_SCOPE_KEY = withDefaultValue("abc");
+    private static final ScopeKey<TestEnum> ENUM_SCOPE_KEY = withDefaultValue(ABC);
+    private static final ScopeKey<Boolean> BOOLEAN_SCOPE_KEY = withDefaultValue(true);
+    private static final ScopeKey<String> SOME_SCOPE_KEY = allocate();
+
+    @Test
+    void testApiCompile() {
+        assertEquals(Integer.valueOf(1), INT_SCOPE_KEY.get());
+        assertEquals(Long.valueOf(1), LONG_SCOPE_KEY.get());
+        assertEquals("abc", STRING_SCOPE_KEY.get());
+        assertSame(ABC, ENUM_SCOPE_KEY.get());
+        assertTrue(BOOLEAN_SCOPE_KEY.get());
+        assertTrue(LIST_SCOPE_KEY.get().isEmpty());
+        LIST_SCOPE_KEY.get().add("test");
+        assertFalse(LIST_SCOPE_KEY.get().isEmpty());
+        assertNull(SOME_SCOPE_KEY.get());
+    }
+
+    enum TestEnum {
+        ABC
+    }
+}
