68b107c8c6b4af0863bb7b8c0a63e0fa9408b67d
wangzhiqian <wangzhiqian@kuaishou.com>
2019-01-20 20:29:10 +0800

    完善 ScopeAsyncRetry

--- src/main/java/com/github/phantomthief/scope/ScopeAsyncRetry.java
+++ src/main/java/com/github/phantomthief/scope/ScopeAsyncRetry.java
@@ -4,31 +4,48 @@ import static com.github.phantomthief.scope.Scope.getCurrentScope;
 import static com.github.phantomthief.scope.Scope.supplyWithExistScope;
 import static com.google.common.util.concurrent.Futures.catchingAsync;
 import static com.google.common.util.concurrent.MoreExecutors.listeningDecorator;
+import static java.lang.Thread.MAX_PRIORITY;
+import static java.util.concurrent.Executors.newScheduledThreadPool;
 
-import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
-import java.util.function.Supplier;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.LongSupplier;
 
+import javax.annotation.Nonnegative;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
+import com.github.phantomthief.util.ThrowableSupplier;
 import com.google.common.util.concurrent.FutureCallback;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.ListeningScheduledExecutorService;
 import com.google.common.util.concurrent.SettableFuture;
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 
 /**
- * @author wangzhiqian <wangzhiqian@kuaishou.com>
+ * @author myco
  * Created on 2019-01-20
  */
-public class AsyncRetry {
+public class ScopeAsyncRetry {
 
-    private static ListeningScheduledExecutorService scheduler = listeningDecorator(
-            Executors.newScheduledThreadPool(10));
+    private final ListeningScheduledExecutorService scheduler;
 
-    private static <T> FutureCallback setResultToOtherSettableFuture(SettableFuture<T> target) {
+    public static ScopeAsyncRetry create(@Nonnegative ScheduledExecutorService executor) {
+        return new ScopeAsyncRetry(executor);
+    }
+
+    public static ScopeAsyncRetry shared() {
+        return LazyHolder.INSTANCE;
+    }
+
+    private ScopeAsyncRetry(ScheduledExecutorService scheduler) {
+        this.scheduler = listeningDecorator(scheduler);
+    }
+
+    private static <T> FutureCallback<T> setResultToOtherSettableFuture(SettableFuture<T> target) {
         return new FutureCallback<T>() {
 
             @Override
@@ -46,43 +63,48 @@ public class AsyncRetry {
     /**
      * 带重试的调用
      *
-     * @param func 需要重试的调用
      * @param singleCallTimeout 单次调用超时限制，单位：ms
-     * @param retryTime 重试次数
-     * @param retryInterval 重试间隔，单位：ms
+     * @param func 需要重试的调用
      * @return 带重试的future
      */
-    public static <T> ListenableFuture<T> withRetry(@Nonnull Supplier<ListenableFuture<T>> func,
-            long singleCallTimeout, int retryTime, long retryInterval) {
+    public <T, X extends Throwable> ListenableFuture<T> retry(long singleCallTimeout,
+            RetryPolicy retryPolicy, @Nonnull ThrowableSupplier<ListenableFuture<T>, X> func) {
         SettableFuture<T> resultFuture = SettableFuture.create();
-        return callWithRetry(func, singleCallTimeout, retryTime, retryInterval, resultFuture);
+
+        AtomicInteger retryTime = new AtomicInteger(0);
+        LongSupplier retryIntervalSupplier = () -> retryPolicy.retry(retryTime.incrementAndGet());
+
+        Scope scope = getCurrentScope();
+        ThrowableSupplier<ListenableFuture<T>, X> scopeWarppedFunc = () -> supplyWithExistScope(
+                scope, func);
+
+        return callWithRetry(scopeWarppedFunc, singleCallTimeout, retryIntervalSupplier,
+                resultFuture);
     }
 
     /**
      * 内部递归方法，返回值是最终的挂了多个retry callback的future
      */
-    private static <T> SettableFuture<T> callWithRetry(@Nonnull Supplier<ListenableFuture<T>> func,
-            long singleCallTimeout, int retryTime, long retryInterval,
-            SettableFuture<T> resultFuture) {
+    private <T, X extends Throwable> SettableFuture<T> callWithRetry(
+            @Nonnull ThrowableSupplier<ListenableFuture<T>, X> func, long singleCallTimeout,
+            LongSupplier retryIntervalSupplier, SettableFuture<T> resultFuture) {
 
-        Scope scope = getCurrentScope();
         // 开始当前一次调用尝试
-        final SettableFuture<T> currentTry = supplyWithExistScope(scope, () -> {
-            SettableFuture<T> future = SettableFuture.create();
-            try {
-                Futures.addCallback(func.get(), setResultToOtherSettableFuture(future));
-            } catch (Throwable t) {
-                future.setException(t);
-            }
-            return future;
-        });
+        final SettableFuture<T> currentTry = SettableFuture.create();
+        try {
+            Futures.addCallback(func.get(), setResultToOtherSettableFuture(currentTry));
+        } catch (Throwable t) {
+            currentTry.setException(t);
+        }
         // 看是先超时还是先执行完成或者执行抛异常
         scheduler.schedule(() -> {
             currentTry.setException(new TimeoutException());
             currentTry.cancel(false);
         }, singleCallTimeout, TimeUnit.MILLISECONDS);
 
-        if (retryTime <= 0) {
+        long retryInterval = retryIntervalSupplier.getAsLong();
+
+        if (retryInterval < 0) {
             // 如果不会再重试了，那就不管什么结果都set到最终结果里吧
             Futures.addCallback(currentTry, setResultToOtherSettableFuture(resultFuture));
         } else {
@@ -92,7 +114,7 @@ public class AsyncRetry {
 
         // 为了用lambda ...
         SettableFuture<T> resultFuture0 = resultFuture;
-        if (retryTime > 0) {
+        if (retryInterval >= 0) {
             // SettableFuture类型不好处理，后边也用不到set了
             ListenableFuture<T> currentTry0 = currentTry;
             if (retryInterval > 0) {
@@ -104,12 +126,22 @@ public class AsyncRetry {
             }
             // 挂上重试任务
             catchingAsync(currentTry0, Throwable.class, x -> callWithRetry(func, singleCallTimeout,
-                    retryTime - 1, retryInterval, resultFuture0));
+                    retryIntervalSupplier, resultFuture0));
         }
 
         return resultFuture;
     }
 
     private static class RetryIntervalSignal extends TimeoutException {
+    }
+
+    private static final class LazyHolder {
+
+        private static final ScopeAsyncRetry INSTANCE = create(
+                newScheduledThreadPool(Runtime.getRuntime().availableProcessors(),
+                        new ThreadFactoryBuilder() //
+                                .setPriority(MAX_PRIORITY) //
+                                .setNameFormat("default-retrier-%d") //
+                                .build()));
     }
 }
