9c574a0ef0addd2467cd350f9df8575d991ad55d
w.vela <boywang004@hotmail.com>
2020-06-16 14:26:50 +0800

    test(common): enable fast thread local test.

--- src/test/java/com/github/phantomthief/scope/FastThreadLocalEnabledTest.java
+++ src/test/java/com/github/phantomthief/scope/FastThreadLocalEnabledTest.java
@@ -4,7 +4,6 @@ import static com.github.phantomthief.scope.MyThreadLocalFactory.USE_FAST_THREAD
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -16,7 +15,6 @@ class FastThreadLocalEnabledTest {
     /**
      * 由于netty依赖是optional的，所以这个测试用例只在IDEA中手工运行确认
      */
-    @Disabled
     @Test
     void test() {
         System.setProperty(USE_FAST_THREAD_LOCAL, "true");
